a9a294f3572bc5619c6a1167a8f440c0e1bab47e
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-06-05 00:28:32 +0530

    added useState and createLogin

--- App.tsx
+++ App.tsx
@@ -1,118 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, View, useAnimatedValue } from 'react-native'
+import React, { useState } from 'react'
+import * as Yup from 'yup'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const PasswordSchema=Yup.object().shape({
+  passwordlength:Yup.number()
+  .min(4,"Should be minimum of 4 characters")
+  .max(16,"Should not be more than 16 characters")
+  .required('Length is required')
+})
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// const  [password,setpassword]=useState('')
+// const [isPassGenerated,setIsPassGenerated]=useState(false)
+// const [lowerCase,setLowerCase]=useState(true)
+// const [upperCase,setUpperCase]=useState(true)
+// const [NumberSchema,useNumbers]=useState(false)
+// const [symbols,useSymbols]=useState(false)
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+// const generatedPasswordString=(paswordLength :number) => 
+//   {
+//     //
+//   }
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+//   const createPaswword= (characters:string,passwordLength:number) =>  
+//     {
+//       let result='';
+//       for (let index = 0; index < passwordLength; index++) 
+//       {
+//         const characterIndex= Math.round(Math.random() *characters.length) 
+//         result +=characters[characterIndex]
+        
+//       }
+//       return result 
+//     }
+
+//     const resetPasswordState= () =>
+//       {
+//         //
+//       }
+
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text>App</Text>
+        
     </View>
-  );
+  )
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+const styles = StyleSheet.create({})
